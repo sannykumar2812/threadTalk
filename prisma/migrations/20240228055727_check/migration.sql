5d4aca56fc0a7deb09fbc92771f66cb6f29e6e4d
Sanny Kumar <sannykumar@Sannys-MacBook-Pro.local>
2024-02-28 11:35:44 +0530

    minor changs

---
+++ prisma/migrations/20240228055727_check/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT,
+    "email" TEXT,
+    "emailVerified" TIMESTAMP(3),
+    "username" TEXT,
+    "image" TEXT,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
